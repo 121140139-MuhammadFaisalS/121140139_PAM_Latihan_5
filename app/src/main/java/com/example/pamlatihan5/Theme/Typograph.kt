792cc3d3ae12a90c5bf75c56b799a82295c6dbbb
121140139-MuhammadFaisalS <162481555+121140139-MuhammadFaisalS@users.noreply.github.com>
2024-05-24 22:38:10 +0700

    Add files via upload

---
+++ app/src/main/java/com/example/pamlatihan5/Theme/Typograph.kt
@@ -0,0 +1,17 @@
+package com.example.pamlatihan5.Theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.sp
+
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontFamily = FontFamily.Default,
+        lineHeight = 24.sp,
+        fontSize = 16.sp,
+        letterSpacing = 0.5.sp
+    )
+)
